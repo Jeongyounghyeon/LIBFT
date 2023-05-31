f0faf49b3853befd69386813c24de2b0ef526fd4
Jeongyounghyeon <jcs5150@naver.com>
2023-05-31 16:54:27 +0900

    add ft_strjoin

--- src/include/libft.h
+++ src/include/libft.h
@@ -8,5 +8,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
