66f0016912543225ed9aec359775f3a799f56059
Jeongyounghyeon <jcs5150@naver.com>
2023-05-31 16:55:10 +0900

    add ft_striteri

--- src/include/libft.h
+++ src/include/libft.h
@@ -9,5 +9,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
