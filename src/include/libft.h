c2a7831634a844e0c1dfd522f44950b7ae961404
Jeongyounghyeon <jcs5150@naver.com>
2023-05-31 19:56:01 +0900

    add ft_lstadd_front

--- src/include/libft.h
+++ src/include/libft.h
@@ -35,5 +35,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
