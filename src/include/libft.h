d2b6777deab777486720a560fef702c3a49d538d
Jeongyounghyeon <jcs5150@naver.com>
2023-05-31 16:57:52 +0900

    add ft_strncmp

--- src/include/libft.h
+++ src/include/libft.h
@@ -12,5 +12,6 @@ char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strdup(const char *s1);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
