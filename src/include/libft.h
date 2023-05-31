2d6db24440deee4874dfa190c2a1faf5c1d87eb8
Jeongyounghyeon <jcs5150@naver.com>
2023-05-31 20:05:21 +0900

    add ft_putchar_fd

--- src/include/libft.h
+++ src/include/libft.h
@@ -33,5 +33,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+int		ft_putchar_fd(char c, int fd);
 
 #endif
