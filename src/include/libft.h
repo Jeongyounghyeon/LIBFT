0cdcde8d9dd591e8a387d004dcc6f7d0ba39cd26
Jeongyounghyeon <jcs5150@naver.com>
2023-05-31 16:53:09 +0900

    add ft_strchr

--- src/include/libft.h
+++ src/include/libft.h
@@ -6,5 +6,6 @@
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
 
 #endif
